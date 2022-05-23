4da599d4dfce7f62acddb0e0263e4079e699c016
Luis H <devlhm05@gmail.com>
2022-05-23 11:57:29 -0300

    added bg anim

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
-import { updateKilometersLabel, renderUpgrades, updateKmPerSecLabel } from "./modules/ui.js";
+import { updateKilometersLabel, renderUpgrades, updateKmPerSecLabel, bgAnimTimer } from "./modules/ui.js";
 import { Upgrade, UpgradeTypes } from "./modules/upgrade.js";
 
 let kilometers = 0;
 let deltaKm = 0;
 
 const upgrades = [
-	new Upgrade(
-		UpgradeTypes.INCREASE_CLICK_AMOUNT,
-		"Pneus Novos",
-		"Aumenta a quantidade andada por clique em 2!",
-		2,
-		10
-	),
-	new Upgrade(
-		UpgradeTypes.INCREASE_PASSIVE_INCOME,
-		"Tijolo no Acelerador!",
-		"Aumenta a quantidade andada passivamente em 10!",
-		10,
-		100
-	),
+    new Upgrade(
+        UpgradeTypes.INCREASE_CLICK_AMOUNT,
+        "Pneus Novos",
+        "Aumenta a quantidade andada por clique em 2!",
+        2,
+        10
+    ),
+    new Upgrade(
+        UpgradeTypes.INCREASE_PASSIVE_INCOME,
+        "Tijolo no Acelerador!",
+        "Aumenta a quantidade andada passivamente em 10!",
+        10,
+        100
+    ),
 ];
 
 renderUpgrades(upgrades);
+bgAnimTimer();
 
 function updateKilometers(newKm) {
-	const difference = newKm - kilometers
-	deltaKm += difference > 0 ? difference : 0;
-	kilometers = newKm;
-	updateKilometersLabel(deltaKm);
+    const difference = newKm - kilometers
+    deltaKm += difference > 0 ? difference : 0;
+    kilometers = newKm;
+    updateKilometersLabel(deltaKm);
 }
 
 setInterval(() => {
-	updateKmPerSecLabel(deltaKm);
-	deltaKm = 0;
+    updateKmPerSecLabel(deltaKm);
+    deltaKm = 0;
 }, 1000);
 
 const increaseKilometers = (factor) => updateKilometers(kilometers + factor);
 const decreaseKilometers = (factor) => updateKilometers(kilometers - factor);
 
 export {
-	updateKilometers,
-	increaseKilometers,
-	decreaseKilometers,
-	kilometers,
-	upgrades,
+    updateKilometers,
+    increaseKilometers,
+    decreaseKilometers,
+    kilometers,
+    upgrades,
 };
--- modules/ui.js
+++ modules/ui.js
@@ -2,44 +2,59 @@ import { kilometers, upgrades } from "../index.js";
 import { upgradeBought } from "./input.js";
 
 function renderUpgrades(upgrades) {
-	const upgradeContainer = document.getElementById("upgrade-container");
-	const upgradeTemplate = document.getElementById("upgrade-template");
+    const upgradeContainer = document.getElementById("upgrade-container");
+    const upgradeTemplate = document.getElementById("upgrade-template");
 
-	const title = upgradeTemplate.content.querySelector("h1");
-	const description = upgradeTemplate.content.querySelector(".description");
-	const button = upgradeTemplate.content.querySelector("button");
-	const container = upgradeTemplate.content.querySelector("div");
+    const title = upgradeTemplate.content.querySelector("h1");
+    const description = upgradeTemplate.content.querySelector(".description");
+    const button = upgradeTemplate.content.querySelector("button");
+    const container = upgradeTemplate.content.querySelector("div");
 
-	for (let index = 0; index < upgrades.length; index++) {
-		const upgrade = upgrades[index];
-		title.innerText = upgrade.title;
-		description.innerText = upgrade.description;
-		button.innerText = upgrade.price + " km";
-		container.dataset.upgradeIndex = index;
+    for (let index = 0; index < upgrades.length; index++) {
+        const upgrade = upgrades[index];
+        title.innerText = upgrade.title;
+        description.innerText = upgrade.description;
+        button.innerText = upgrade.price + " km";
+        container.dataset.upgradeIndex = index;
 
-		const clone = document.importNode(upgradeTemplate.content, true);
-		const cloneButton = clone.querySelector("button");
+        const clone = document.importNode(upgradeTemplate.content, true);
+        const cloneButton = clone.querySelector("button");
 
-		cloneButton.addEventListener("click", () => upgradeBought(index));
+        cloneButton.addEventListener("click", () => upgradeBought(index));
 
-		upgradeContainer.appendChild(clone);
-	}
+        upgradeContainer.appendChild(clone);
+    }
+}
+
+function bgAnimTimer() {
+    moveBg();
+    setTimeout(() => bgAnimTimer(), 10);
+}
+
+let bgOffset = 0;
+let bgSpeed = 0.5;
+
+function moveBg() {
+    const bg = document.getElementById('bg-container');
+    bgOffset = bgOffset - bgSpeed <= -1000 ? 0 : bgOffset - bgSpeed;
+
+    bg.style.backgroundPositionX = bgOffset + "%";
 }
 
 function updateUpgradePriceLabel(upgrade) {
-	const index = upgrades.indexOf(upgrade);
-	const upgradeContainer = document.querySelector(`[data-upgrade-index="${index}"]`)
-	upgradeContainer.querySelector("button").innerText = upgrade.price + " km";
-	upgradeContainer.querySelector(".level").innerText = "Nível " + upgrade.level;
-	
+    const index = upgrades.indexOf(upgrade);
+    const upgradeContainer = document.querySelector(`[data-upgrade-index="${index}"]`)
+    upgradeContainer.querySelector("button").innerText = upgrade.price + " km";
+    upgradeContainer.querySelector(".level").innerText = "Nível " + upgrade.level;
+
 }
 
 function updateKilometersLabel() {
-	document.getElementById("kilometers-label").innerText = kilometers + " km";
+    document.getElementById("kilometers-label").innerText = kilometers + " km";
 }
 
 function updateKmPerSecLabel(deltaKm) {
-	document.getElementById("km-per-sec-label").innerText = deltaKm + " km per second";
+    document.getElementById("km-per-sec-label").innerText = deltaKm + " km per second";
 }
 
-export { updateKilometersLabel, renderUpgrades, updateKmPerSecLabel, updateUpgradePriceLabel };
+export { updateKilometersLabel, renderUpgrades, updateKmPerSecLabel, updateUpgradePriceLabel, bgAnimTimer };
